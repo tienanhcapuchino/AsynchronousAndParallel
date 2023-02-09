ed25ca94bb5518326c487782f4c76d59e6cda214
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-09 10:20:13 +0700

    Add project files.

---
+++ AsynchronousDemon03/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsynchronousDemon03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private readonly HttpClient client = new HttpClient
+        {
+            MaxResponseContentBufferSize = 1_000_000
+        };
+        private readonly IEnumerable<string> UrlList = new string[] {
+            "https://docs.microsoft.com",
+            "https://docs.microsoft.com/azure",
+            "https://docs.microsoft.com/powershell",
+            "https://docs.microsoft.com/dotnet",
+            "https://docs.microsoft.com/aspnet/core",
+            "https://docs.microsoft.com/windows"
+        };
+        private async void OnStartButtonClick(object sender, RoutedEventArgs e)
+        {
+            btnStartButton.IsEnabled = false;
+            txtResults.Clear();
+            await SumPageSizesAsync();
+            txtResults.Text += $"\nControl returned to {nameof(OnStartButtonClick)}.";
+            btnStartButton.IsEnabled = true;
+        }
+        private async Task SumPageSizesAsync()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            int total = 0;
+            foreach (string url in UrlList)
+            {
+                int contentLength = await ProcessUrlAsync(url, client);
+                total += contentLength;
+            }
+            stopwatch.Stop();
+            txtResults.Text += $"\nTotal bytes returned: {total:#,#}";
+            txtResults.Text += $"\nElapsed time: {stopwatch.Elapsed}\n";
+        }
+        private async Task<int> ProcessUrlAsync(string url, HttpClient client)
+        {
+            byte[] content = await client.GetByteArrayAsync(url);
+            DisplayResults(url, content);
+            return content.Length;
+        }
+        private void DisplayResults(string url, byte[] content) =>
+        txtResults.Text += $"{url,-60} {content.Length,10:#,#}\n";
+        protected override void OnClosed(EventArgs e) => client.Dispose();
+    }
+}
---
+++ TaskDemo04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var tasks = new List<Task<int>>();
+        var source = new CancellationTokenSource();
+        var token = source.Token;
+        int conpletedIterations = 0;
+
+        for (int n = 1; n <= 20; n++)
+        {
+            tasks.Add(Task.Run(() =>
+            {
+                int iterations = 0;
+                for (int ctr = 1; ctr <= 2_000; ctr++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    iterations++;
+                }
+                Interlocked.Increment(ref conpletedIterations);
+                if (conpletedIterations >= 10)
+                    source.Cancel();
+                return iterations;
+            }, token));
+        }
+        Console.WriteLine("Waiting for the first 10 tasks to complete...\n");
+        try
+        {
+            Task.WaitAll(tasks.ToArray());
+        }
+        catch (AggregateException)
+        {
+            Console.WriteLine("status of tasks: \n");
+            Console.WriteLine("{0,10} {1,20} {2,14:N0}", "Task Id","Status", "Iterations");
+            foreach (var t in tasks)
+                Console.WriteLine("{0,10} {1,26} {2,14}",
+                t.Id, t.Status, 
+                t.Status != TaskStatus.Canceled ?
+                t.Result.ToString("N0") : "n/a");
+        }
+        Console.ReadLine();
+    }//end Main
+}//end Program
